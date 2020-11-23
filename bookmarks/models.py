44acebec1d291f7174b04225666a14d83417402e
Jedrzej Czechowicz <emild9876@gmail.com>
2020-11-23 20:11:01 +0100

    9

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,2 +1,10 @@
+from datetime import datetime
 
+from bookmarks import db
+
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    description = db.Column(db.String(300))
 
